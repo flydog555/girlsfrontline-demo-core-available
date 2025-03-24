17479142e5a6fd3b7a3877a6f8027134389f0ca2
flydog555 <wang2006_official@emails.bjut.edu.cn>
2025-03-24 10:47:55 +0800

    #新增敌人击杀效果

--- core/function.cpp
+++ core/function.cpp
@@ -49,6 +49,7 @@ int* enemy_active = &ex;//敌人是否存活
 int tsign = 0;//记录线程循环次数
 int enemyflame = 0;//敌人移动动画帧
 int enemyflame_die = 0;//敌人死亡动画帧
+int enemyflame_attack = 0;//敌人血量
 int Isleft = 0;//敌人移动方向
 
 //子弹
@@ -454,6 +455,158 @@ const char* golyat_die_right[] = {
     "./resource/enemy/golyat/die_right/(46).png",
     "./resource/enemy/golyat/die_right/(47).png"
 };
+const char* golyat_attack_left[] = {
+    "./resource/enemy/golyat/attack_left/ (1).png",
+	"./resource/enemy/golyat/attack_left/ (2).png",
+	"./resource/enemy/golyat/attack_left/ (3).png",
+	"./resource/enemy/golyat/attack_left/ (4).png",
+	"./resource/enemy/golyat/attack_left/ (5).png",
+	"./resource/enemy/golyat/attack_left/ (6).png",
+	"./resource/enemy/golyat/attack_left/ (7).png",
+	"./resource/enemy/golyat/attack_left/ (8).png",
+	"./resource/enemy/golyat/attack_left/ (9).png",
+	"./resource/enemy/golyat/attack_left/ (10).png",
+	"./resource/enemy/golyat/attack_left/ (11).png",
+	"./resource/enemy/golyat/attack_left/ (12).png",
+	"./resource/enemy/golyat/attack_left/ (13).png",
+	"./resource/enemy/golyat/attack_left/ (14).png",
+	"./resource/enemy/golyat/attack_left/ (15).png",
+	"./resource/enemy/golyat/attack_left/ (16).png",
+	"./resource/enemy/golyat/attack_left/ (17).png",
+	"./resource/enemy/golyat/attack_left/ (18).png",
+	"./resource/enemy/golyat/attack_left/ (19).png",
+	"./resource/enemy/golyat/attack_left/ (20).png",
+	"./resource/enemy/golyat/attack_left/ (21).png",
+	"./resource/enemy/golyat/attack_left/ (22).png",
+	"./resource/enemy/golyat/attack_left/ (23).png",
+	"./resource/enemy/golyat/attack_left/ (24).png",
+	"./resource/enemy/golyat/attack_left/ (25).png",
+	"./resource/enemy/golyat/attack_left/ (26).png",
+	"./resource/enemy/golyat/attack_left/ (27).png",
+	"./resource/enemy/golyat/attack_left/ (28).png",
+	"./resource/enemy/golyat/attack_left/ (29).png",
+	"./resource/enemy/golyat/attack_left/ (30).png",
+	"./resource/enemy/golyat/attack_left/ (31).png",
+	"./resource/enemy/golyat/attack_left/ (32).png",
+	"./resource/enemy/golyat/attack_left/ (33).png",
+	"./resource/enemy/golyat/attack_left/ (34).png",
+	"./resource/enemy/golyat/attack_left/ (35).png",
+	"./resource/enemy/golyat/attack_left/ (36).png",
+	"./resource/enemy/golyat/attack_left/ (37).png",
+	"./resource/enemy/golyat/attack_left/ (38).png",
+	"./resource/enemy/golyat/attack_left/ (39).png",
+	"./resource/enemy/golyat/attack_left/ (40).png",
+	"./resource/enemy/golyat/attack_left/ (41).png",
+	"./resource/enemy/golyat/attack_left/ (42).png",
+	"./resource/enemy/golyat/attack_left/ (43).png",
+	"./resource/enemy/golyat/attack_left/ (44).png",
+	"./resource/enemy/golyat/attack_left/ (45).png",
+	"./resource/enemy/golyat/attack_left/ (46).png",
+	"./resource/enemy/golyat/attack_left/ (47).png",
+	"./resource/enemy/golyat/attack_left/ (48).png",
+	"./resource/enemy/golyat/attack_left/ (49).png",
+	"./resource/enemy/golyat/attack_left/ (50).png",
+	"./resource/enemy/golyat/attack_left/ (51).png",
+	"./resource/enemy/golyat/attack_left/ (52).png",
+	"./resource/enemy/golyat/attack_left/ (53).png",
+	"./resource/enemy/golyat/attack_left/ (54).png",
+	"./resource/enemy/golyat/attack_left/ (55).png",
+	"./resource/enemy/golyat/attack_left/ (56).png",
+	"./resource/enemy/golyat/attack_left/ (57).png",
+	"./resource/enemy/golyat/attack_left/ (58).png",
+	"./resource/enemy/golyat/attack_left/ (59).png",
+	"./resource/enemy/golyat/attack_left/ (60).png",
+	"./resource/enemy/golyat/attack_left/ (61).png",
+	"./resource/enemy/golyat/attack_left/ (62).png",
+	"./resource/enemy/golyat/attack_left/ (63).png",
+	"./resource/enemy/golyat/attack_left/ (64).png",
+	"./resource/enemy/golyat/attack_left/ (65).png",
+	"./resource/enemy/golyat/attack_left/ (66).png",
+	"./resource/enemy/golyat/attack_left/ (67).png",
+	"./resource/enemy/golyat/attack_left/ (68).png",
+	"./resource/enemy/golyat/attack_left/ (69).png",
+	"./resource/enemy/golyat/attack_left/ (70).png",
+	"./resource/enemy/golyat/attack_left/ (71).png",
+	"./resource/enemy/golyat/attack_left/ (72).png",
+	"./resource/enemy/golyat/attack_left/ (73).png",
+	"./resource/enemy/golyat/attack_left/ (74).png"
+};
+const char* golyat_attack_right[] = {
+    "./resource/enemy/golyat/attack_right/(1).png",
+    "./resource/enemy/golyat/attack_right/(2).png",
+    "./resource/enemy/golyat/attack_right/(3).png",
+    "./resource/enemy/golyat/attack_right/(4).png",
+    "./resource/enemy/golyat/attack_right/(5).png",
+    "./resource/enemy/golyat/attack_right/(6).png",
+    "./resource/enemy/golyat/attack_right/(7).png",
+    "./resource/enemy/golyat/attack_right/(8).png",
+    "./resource/enemy/golyat/attack_right/(9).png",
+    "./resource/enemy/golyat/attack_right/(10).png",
+    "./resource/enemy/golyat/attack_right/(11).png",
+    "./resource/enemy/golyat/attack_right/(12).png",
+    "./resource/enemy/golyat/attack_right/(13).png",
+    "./resource/enemy/golyat/attack_right/(14).png",
+    "./resource/enemy/golyat/attack_right/(15).png",
+    "./resource/enemy/golyat/attack_right/(16).png",
+    "./resource/enemy/golyat/attack_right/(17).png",
+    "./resource/enemy/golyat/attack_right/(18).png",
+    "./resource/enemy/golyat/attack_right/(19).png",
+    "./resource/enemy/golyat/attack_right/(20).png",
+    "./resource/enemy/golyat/attack_right/(21).png",
+    "./resource/enemy/golyat/attack_right/(22).png",
+    "./resource/enemy/golyat/attack_right/(23).png",
+    "./resource/enemy/golyat/attack_right/(24).png",
+    "./resource/enemy/golyat/attack_right/(25).png",
+    "./resource/enemy/golyat/attack_right/(26).png",
+    "./resource/enemy/golyat/attack_right/(27).png",
+    "./resource/enemy/golyat/attack_right/(28).png",
+    "./resource/enemy/golyat/attack_right/(29).png",
+    "./resource/enemy/golyat/attack_right/(30).png",
+    "./resource/enemy/golyat/attack_right/(31).png",
+    "./resource/enemy/golyat/attack_right/(32).png",
+    "./resource/enemy/golyat/attack_right/(33).png",
+    "./resource/enemy/golyat/attack_right/(34).png",
+    "./resource/enemy/golyat/attack_right/(35).png",
+    "./resource/enemy/golyat/attack_right/(36).png",
+    "./resource/enemy/golyat/attack_right/(37).png",
+    "./resource/enemy/golyat/attack_right/(38).png",
+    "./resource/enemy/golyat/attack_right/(39).png",
+    "./resource/enemy/golyat/attack_right/(40).png",
+    "./resource/enemy/golyat/attack_right/(41).png",
+    "./resource/enemy/golyat/attack_right/(42).png",
+    "./resource/enemy/golyat/attack_right/(43).png",
+    "./resource/enemy/golyat/attack_right/(44).png",
+    "./resource/enemy/golyat/attack_right/(45).png",
+    "./resource/enemy/golyat/attack_right/(46).png",
+    "./resource/enemy/golyat/attack_right/(47).png",
+    "./resource/enemy/golyat/attack_right/(48).png",
+    "./resource/enemy/golyat/attack_right/(49).png",
+    "./resource/enemy/golyat/attack_right/(50).png",
+    "./resource/enemy/golyat/attack_right/(51).png",
+    "./resource/enemy/golyat/attack_right/(52).png",
+    "./resource/enemy/golyat/attack_right/(53).png",
+    "./resource/enemy/golyat/attack_right/(54).png",
+    "./resource/enemy/golyat/attack_right/(55).png",
+    "./resource/enemy/golyat/attack_right/(56).png",
+    "./resource/enemy/golyat/attack_right/(57).png",
+    "./resource/enemy/golyat/attack_right/(58).png",
+    "./resource/enemy/golyat/attack_right/(59).png",
+    "./resource/enemy/golyat/attack_right/(60).png",
+    "./resource/enemy/golyat/attack_right/(61).png",
+    "./resource/enemy/golyat/attack_right/(62).png",
+    "./resource/enemy/golyat/attack_right/(63).png",
+    "./resource/enemy/golyat/attack_right/(64).png",
+    "./resource/enemy/golyat/attack_right/(65).png",
+    "./resource/enemy/golyat/attack_right/(66).png",
+    "./resource/enemy/golyat/attack_right/(67).png",
+    "./resource/enemy/golyat/attack_right/(68).png",
+    "./resource/enemy/golyat/attack_right/(69).png",
+    "./resource/enemy/golyat/attack_right/(70).png",
+    "./resource/enemy/golyat/attack_right/(71).png",
+    "./resource/enemy/golyat/attack_right/(72).png",
+    "./resource/enemy/golyat/attack_right/(73).png",
+    "./resource/enemy/golyat/attack_right/(74).png"
+};
 
 //自定义函数的声明
 void transparentimage3(IMAGE* dstimg, int x, int y, IMAGE* srcimg);
@@ -508,7 +661,7 @@ void playAnimation(const char* frames[], int frameCount,int a)  //主渲染函数
             transparentimage3(NULL, *bpx, *bpy, &bulletimg);
         }
 		//加载敌人
-        if (*enemy_active)
+        if (*enemy_active==1)
         {
             if (Isleft == 1)
             {
@@ -521,7 +674,7 @@ void playAnimation(const char* frames[], int frameCount,int a)  //主渲染函数
                 transparentimage3(NULL, *dpx, *dpy, &enemyimg);
             }
         }
-        else
+        else if (*enemy_active == 0)
         {
             if (Isleft == 1)
             {
@@ -531,6 +684,19 @@ void playAnimation(const char* frames[], int frameCount,int a)  //主渲染函数
             else
             {
                 loadimage(&enemyimg, golyat_die_right[enemyflame_die]);
+                transparentimage3(NULL, *dpx, *dpy, &enemyimg);
+            }
+        }
+        else if (*enemy_active == 2)
+        {
+            if (Isleft == 1)
+            {
+                loadimage(&enemyimg, golyat_attack_left[enemyflame_attack]);
+                transparentimage3(NULL, *dpx, *dpy, &enemyimg);
+            }
+            else
+            {
+                loadimage(&enemyimg, golyat_attack_right[enemyflame_attack]);
                 transparentimage3(NULL, *dpx, *dpy, &enemyimg);
             }
         }
@@ -725,7 +891,7 @@ void updateBullet(Bullet* bullet) {
             return;
         }
 
-        if (*bullet_active==0)  //还需修改
+        if (*bullet_active==0)  
         {
             bullet->active = 0;
             return;
@@ -779,9 +945,22 @@ void updateEnemy(enemy* enemy) {
         double distance = sqrt(dx * dx + dy * dy);
 
         // 如果敌人接触到人物  
-        if (distance < ENEMY_SPEED) {
-            enemy->active = 0;
+        if (distance-100 < ENEMY_SPEED) {
+            enemy->active = 2;    /////////////////////
             //printf("Killed\n");
+            for (int i = 0; i < sizeof(golyat_attack_left) / sizeof(golyat_attack_left[0]); i++)
+            {
+                start_time_anime = clock();
+                enemyflame_attack = i;
+                frame_time_anime = clock() - start_time_anime;
+                if (i != sizeof(golyat_attack_left) / sizeof(golyat_attack_left[0]) - 1)
+                {
+                    if (anime_fps - frame_time_anime > 0)
+                    {
+                        Sleep(anime_fps - frame_time_anime);
+                    }
+                }
+            }
             live--;
             return;
         }
